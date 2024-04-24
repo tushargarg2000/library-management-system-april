469e140a95f8f8f7b3a5c5288a9650f310c07877
Tushar <tushargarg0110@gmail.com>
2024-04-24 22:04:31 +0530

    Code for library management systems

---
+++ src/main/java/com/acciojob/librarymanagementsystemApril/LibraryManagementSystemAprilApplication.java
@@ -0,0 +1,13 @@
+package com.acciojob.librarymanagementsystemApril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemAprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemAprilApplication.class, args);
+	}
+
+}
